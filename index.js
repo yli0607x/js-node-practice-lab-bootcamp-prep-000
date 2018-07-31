52b8cf5020437db830c3d2b4c04c88c6864fff15
Yu Li <yli0607x@gmail.com>
2018-07-31 00:26:06 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // write your code below!
 
 var name = "YU LI"
-var height = "74"
+var height = 74
 
 console.log("Name:")
 console.log(name)
@@ -9,4 +9,4 @@ console.log(name)
 console.log("Height:")
 console.log(height)
 
-console.log(parseInt(height) + 1 )
+console.log(height + 1 )
